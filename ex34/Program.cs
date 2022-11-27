0fcfba8361df401e4e2c96d799cc15a6a6c4aa5c
nikita <zherko.na@phystech.edu>
2022-11-27 19:52:08 +0300

    Added Exercise 34

---
+++ ex34/Program.cs
@@ -0,0 +1,25 @@
+﻿System.Console.WriteLine("Длина массива: ");
+int ArraySize = Convert.ToInt32(Console.ReadLine());
+
+int[] array = FillArray(ArraySize, 100, 1000);
+System.Console.Write(String.Join(", ", array));
+int res = EvenNumbers(array);
+System.Console.WriteLine($" -> {res}");
+
+////////////////////////////////////////////////////
+int EvenNumbers(int[] FuncArray)
+{
+    int EvenAmount = 0;
+    foreach (int item in FuncArray)
+        if(item % 2 == 0) EvenAmount++;
+    return EvenAmount;
+}
+
+
+int[] FillArray(int size, int MinElement, int MaxElement)
+{
+    int[] FuncArray = new int[size];
+    for (int i = 0; i < size; i++)
+        FuncArray[i] = new Random().Next(MinElement, MaxElement + 1);
+    return FuncArray;
+}
